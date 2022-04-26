eb2764aaefef2c8256d8db2feb6a9f8de2a48d66
i7eo <406132614@qq.com>
2022-04-26 17:53:50 +0800

    feat(error update): error page update

--- pages/_error.js
+++ pages/_error.js
@@ -1,13 +1,35 @@
+import Link from 'next/link'
+
 function Error ({ statusCode }) {
   return (
-    <>
-      <div>i7eo</div>
-      <p>
-        {statusCode
-          ? `An error ${statusCode} occurred on server`
-          : 'An error occurred on client'}
-      </p>
-    </>
+    <div className="error-wrapper">
+      <div
+        className="error-image"
+        style={{
+          width: '120px',
+          height: '120px',
+          background: 'url("https://i7eo.com/fail.png")',
+          backgroundSize: 'cover'
+        }}
+      ></div>
+      <div className="error-desc">
+        <p>
+          <b style={{ color: '#d41717', fontSize: '24px', lineHeight: '30px' }}>
+            Oops ...
+          </b>
+          <span>
+            {"It's embarrassing, but I'm sorry for"}
+            {
+                statusCode ? `${statusCode} error` : 'client error'
+            }
+          </span>
+        </p>
+        <p>
+          <span>{"There's nothing to see here"}</span>
+          <Link href={'/'}>GO BACK</Link>
+        </p>
+      </div>
+    </div>
   )
 }
 
